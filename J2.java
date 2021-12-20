5b8f2e6f8dc45c2040eb9b99cb4e1b8c12c01093
109b29723 <92261548+109b29723@users.noreply.github.com>
2021-12-20 12:04:43 +0800

    Create J2.java

---
+++ J2.java
@@ -0,0 +1,43 @@
+package Java;
+interface iShape03
+{   
+	public void show(); 
+	public int area();
+}
+interface iColor
+{
+	public void showColor();
+}
+class CWin 
+{
+	protected int width;
+	protected int height;
+	protected String color;
+	public CWin(int w,int h,String str)
+	{
+		width = w;
+		height = h;
+		color = str;
+	}
+	public void show()
+	{
+		showColor();
+		System.out.println("width="+width);
+		System.out.println("height="+height);
+		System.out.println("area="+area());
+	}
+	public int area()
+	{
+		return width * height;
+	}
+	public void showColor()
+	{
+		System.out.println("color="+color);
+	}
+}
+public class J2 {
+	public static void main(String[] args) {
+		CWin win=new CWin(5,7,"Green");
+		win.show();
+	}
+}
